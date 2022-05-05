1e4278227689f34aa3596c685fdcfaf6cbaf50ac
ankitpakhale <akp3067@gmail.com>
2022-05-05 22:34:07 +0530

    done

--- Demo1/settings.py
+++ Demo1/settings.py
@@ -122,3 +122,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT  = os.path.join(BASE_DIR, 'media')
+
+
+# Default primary key field type
+# https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
